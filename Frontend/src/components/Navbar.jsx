93300a6d29e9cf176afb9a52fdb757e272f0a0a1
Chungu Muloshi <148175759+Chungu13@users.noreply.github.com>
2026-02-10 12:08:45 +0200

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -126,7 +126,7 @@ const Navbar = () => {
               <p className="px-4 py-2 rounded inline-block">HOME</p>
             </NavLink>
             <NavLink onClick={() => setShowMenu(false)} to="/businesses">
-              <p className="px-4 py-2 rounded inline-block">ALL DOCTORS</p>
+              <p className="px-4 py-2 rounded inline-block">ALL SERVICES</p>
             </NavLink>
             <NavLink onClick={() => setShowMenu(false)} to="/about">
               <p className="px-4 py-2 rounded inline-block">ABOUT</p>
