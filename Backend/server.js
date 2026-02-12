660693d4cd3cc0e8f054c12d12e55fd75c4d395d
Chungu Muloshi <148175759+Chungu13@users.noreply.github.com>
2026-02-13 00:56:38 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -62,7 +62,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", 'atoken', 'dtoken'],
 };
 
 app.use(cors(corsOptions));
